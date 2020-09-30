49647194e0361580fe2d60cca6a17c84afbe4159
Byambadalai Sumiya <byamba4life@gmail.com>
2020-09-30 20:24:19 +0800

    Create tutorial.py

---
+++ Tutorial - 4 - String/tutorial.py
@@ -0,0 +1,7 @@
+tutorial = "Python programming language"
+
+print(tutorial.upper())                           # Том үсгээр хэвлэнэ
+print(tutorial.lower())                           # Жижиг үсгээр хэвлэнэ
+print(tutorial.count('programming'))              # 'programming' гэдэг үг хэд байгааг тоолж байна
+print(tutorial.replace('Python', 'Java'))         # 'Python' үгийг 'Java' үгээр сольж байна
+print(tutorial.index('language'))                 # 'language' үгийн хэддэх index г олно
