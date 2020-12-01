6e9e75aa4cce510098d247ad84f368d87d22e006
ByamB4 <byamba4life@gmail.com>
2020-12-01 16:08:25 +0800

    Хичээл - 10

--- tutorial.py
+++ tutorial.py
@@ -1,24 +1,12 @@
-# Жишээ - 1
-nums = [1, 2, 3, 4, 5, 6, 7]
-# 'nums' list-ийн дагуу гүйж байна
-for i in nums:
-    # Хэрвээ i == 3 байвал програм 3 ийн тоог хэвлэхгүй дараагийн давталт луу орно
-    if i == 3:
-        continue
-    # Хэрвээ i == 5 байвал давталт маань break хийж давталтаас гарна
-    elif i == 5:
-        break
-    print(i)
+def sum_of_numbers(*args, **kwargs):
+    """Will return sum of numbers"""
+    print(kwargs['name'])
+    return sum(args)
 
-# Жишээ - 2
-# 0 - 100 хүртэл бүх тэгш тоог хэвлэж байна
-for i in range(0, 100, 2):
-    print(i)
 
-# Жишээ - 3
-f = 52
-i = 1
-# Давталт эхэлж байна, нөхцөл нь 'f' != 'i' буюу энэ хоёр ялгаатай байх л юм бол үргэлжлээд байна
-while i != f:
-    print(i)
-    i += 1
+teacher = {
+    'name': 'Byambadalai',
+    'more': 'https://facebook.com/ByamB4'
+}
+
+print(sum_of_numbers(6, 2, 3, 4, 5, **teacher))
